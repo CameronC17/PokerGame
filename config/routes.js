dcde5997bedb86dd0442fd7fa2e5fa5832006c67
Adam Flanagan <adam.flanagan@skybettingandgaming.com>
2016-09-26 13:41:52 +0100

    login

--- config/routes.js
+++ config/routes.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var usersController = require('../controllers/users');
+var sessionController = require('../controllers/sessions');
 
+// Session routes
+router.route('/users/login')
+      .post(sessionController.create)
+      .delete(sessionController.delete);
 
+router.route('/sessions/new')
+      .get(sessionController.new);
 
 router.route('/users')
   .post(usersController.create);
