de624d871b4d55c9ac93c1e41175204c04e412bb
Joshua Berry <joshua.berry92@gmail.com>
2016-09-28 12:23:27 +0100

    Added a bit more authentication tings

--- controllers/games.js
+++ controllers/games.js
@@ -1,20 +1,34 @@
 var Game = require('../game/game.js');
 var Player = require('../game/player.js');
 var tables = [];
-
+var User = require('../models/user')
 function joinGame(req, res) {
 
 }
 
 function controlGame(req, res) {
   //checks what user we are playing as
-  console.log(req.body.user);
-  if(req.session.user != undefined) {
-    console.log(req.body);
+  // console.log(req.body.user);
+  if(req.body.user != undefined) {
+    User.findOne({ _id: req.body.user }, function(err, user) {
+      if (user) {
+          // Sets the session user id to equal the logged in user id.
+          console.log(user.username);
+          res.sendStatus(200);
+      } else {
+          if (err) {
+              console.log(err.message);
+          } else {
+              console.log("There's no user with those credentials!");
+          }
+          res.sendStatus(400);
+      }
+  });
   } else {
     console.log("User not logged in!");
+    res.sendStatus(403);
   }
-  res.sendStatus(200);
+
 }
 
 function createGame(req, res) {
