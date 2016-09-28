1a212020912a4b234e967014b0d1840eceb2bb2f
Joshua Berry <joshua.berry92@gmail.com>
2016-09-28 12:28:15 +0100

    worked more on authentication

--- controllers/games.js
+++ controllers/games.js
@@ -10,20 +10,7 @@ function controlGame(req, res) {
   //checks what user we are playing as
   // console.log(req.body.user);
   if(req.body.user != undefined) {
-    User.findOne({ _id: req.body.user }, function(err, user) {
-      if (user) {
-          // Sets the session user id to equal the logged in user id.
-          console.log(user.username);
-          res.sendStatus(200);
-      } else {
-          if (err) {
-              console.log(err.message);
-          } else {
-              console.log("There's no user with those credentials!");
-          }
-          res.sendStatus(400);
-      }
-  });
+
   } else {
     console.log("User not logged in!");
     res.sendStatus(403);
@@ -31,12 +18,34 @@ function controlGame(req, res) {
 
 }
 
+function getUserCredentials(userID){
+  var credentials = [];
+  User.findOne({ _id: userID }, function(err, user) {
+    if (user) {
+        // Sets the session user id to equal the logged in user id.
+        console.log(user.username);
+        res.sendStatus(200);
+    } else {
+        if (err) {
+            console.log(err.message);
+        } else {
+            console.log("There's no user with those credentials!");
+        }
+        res.sendStatus(400);
+    }
+});
+
+}
+
+
 function createGame(req, res) {
   var players = [];
 
-  for (var i = 0; i < 5; i++){
-    players.push(new Player());
-  }
+  // for (var i = 0; i < 5; i++){
+  //   players.push(new Player());
+  // }
+  // function Player(username, id, wallet, hand, handValue)
+  players.push(new Player())
   tables.push(new Game(players));
 
   var game = tables[tables.length - 1];
