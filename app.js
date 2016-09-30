5e355bba159962597b9024fcfdef808323874b36
Joshua Berry <joshua.berry92@gmail.com>
2016-09-30 11:22:37 +0100

    Fixing head

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use(bodyParser.json());
 
-<<<<<<< HEAD
+
 // mongoose.connect('mongodb://localhost/poker');
 mongoose.connect(process.env.DB_URL || 'mongodb://192.10.10.200:27017/poker');
 //mongoose.connect('mongodb://localhost/poker');
